da637846a0cb53c610839ac89dcf71ae7d5f6d4b
Jakub 'Eremiell' Marek <eremiell@eremiell.net>
2017-09-08 19:22:13 +0200

    Adds list program.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 #!/usr/bin/make -f
 
+list:	list.cpp
+	g++ -std=c++14 -O2 -Wall -Wextra -Wpedantic -olist list.cpp
 bin:	main.cpp
 	g++ -std=c++14 -O2 -Wall -Wextra -Wpedantic -oeremario-1 main.cpp
 clang:	main.cpp
---
+++ list.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <string>
+
+void view(std::vector<std::string> &list) {
+	for (std::size_t i = 0; i < list.size(); ++i) {
+		std::cout << '[' << i << "] " << list.at(i) << '\n';
+	}
+	return;
+}
+
+void add(std::vector<std::string> &list) {
+	std::cout << "Input item to add to list: ";
+	std::string s;
+	std::getline(std::cin, s);
+	list.emplace_back(s);
+	return;
+}
+
+void del(std::vector<std::string> &list) {
+	view(list);
+	std::cout << "Enter item number to delete: ";
+	std::size_t item;
+	std::cin >> item;
+	if (item < list.size()) {
+		list.erase(list.begin() + item);
+	}
+	else {
+		std::cout << "Wrong item number: " << item << "\n";
+	}
+	return;
+}
+
+int main() {
+	std::vector<std::string> list;
+	bool again = true;
+	while (again) {
+		bool read = false;
+		while (!read) {
+			std::cout << "Add[A]/Delete[D]/View[V]/Quit[Q]: ";
+			char input;
+			std::cin >> input;
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			switch (input) {
+				case 'a':
+				case 'A':
+					read = true;
+					add(list);
+					break;
+				case 'd':
+				case 'D':
+					read = true;
+					del(list);
+					break;
+				case 'v':
+				case 'V':
+					read = true;
+					view(list);
+					break;
+				case 'q':
+				case 'Q':
+					read = true;
+					again = false;
+					break;
+				default:
+					std::cout << "Wrong option selected: " << input << '\n';
+					break;
+			}
+		}
+	}
+	return 0;
+}
